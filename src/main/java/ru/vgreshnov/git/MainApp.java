a70c6c032b74cd38b80bf6b7c8803915466c8482
Vladimir Greshnov <vgreshnov@gmail.com>
2025-08-05 18:09:18 +0300

    Second commit

--- src/main/java/ru/vgreshnov/git/MainApp.java
+++ src/main/java/ru/vgreshnov/git/MainApp.java
@@ -3,5 +3,6 @@ package ru.vgreshnov.git;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
